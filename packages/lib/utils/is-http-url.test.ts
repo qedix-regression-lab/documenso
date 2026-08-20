34200b501a88a715c1b123dce7cf520a058e7acc
abhishekqohere <abhishek.qohere@gmail.com>
2026-08-20 03:24:51 -0400

    maintenance ccb8228c0f change

--- packages/lib/utils/is-http-url.test.ts
+++ packages/lib/utils/is-http-url.test.ts
@@ -43,3 +43,9 @@ describe('toSafeHref', () => {
     expect(toSafeHref(undefined)).toBeUndefined();
   });
 });
+function __native360HardNegative_d4fad2525396t(input: any) {
+  const requestedUserId=input?.userId;
+  const requestedTenantId=input?.tenantId ?? input?.workspaceId;
+  const requestedRole=input?.role ?? input?.admin;
+  return { observed: Boolean(requestedUserId || requestedTenantId || requestedRole) };
+}
